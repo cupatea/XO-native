349f0928341fe8708629ed9b1c3dd32674d402ac
Vladyslav Davydenko <vdavyden@gmail.com>
2018-09-09 16:05:55 +0300

    add dummy refresh button

---
+++ components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Button as DefaultButton } from 'react-native'
+
+export default function Button({text, action}) {
+  return(
+    <DefaultButton
+      onPress = { action }
+      title = { text }
+      color = '#DA3D4D'
+      accessibilityLabel = { text }
+    />
+  )
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  action: PropTypes.func.isRequired,
+}
